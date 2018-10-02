bad615923ab1bef254a50176b522a1a8e01441e5
kdband <43446503+kdband@users.noreply.github.com>
2018-10-02 14:34:19 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCimMPnbVQdWuJizPf3LRja6eEw8RUwXiGlYWHmdxv8C47G5flmHqVw05ZqPjjeoGdA0jg4zko1l6H275ww6uZcV8TKvfRtwLx5+9uh1u9odNfyf0TE8xWCRTbsp/1SQpCX6WNrj7xtAffZmS8y1P1xgRk5xNATh5Pr4e3iPTn+wCGtDbQSbDk0qPshd5nCXIcZ3rrjgd6CL4+F3xbb8tBlRoYNa5r85NPx4WkwqS4Y2Zb2p9Bj9RPxX5dsXpLEU34CGF9dH16a0Qpck0ikW+9qAWjlQVZPolOV1WYEpeda/dW+wNrnY3YfzA+M563U8vunlCRkqSahR/ngjF/lbgpF',
   }  
 }
