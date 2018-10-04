d0d4ad870781b182511e05735628f4702593f46b
kdband <43446503+kdband@users.noreply.github.com>
2018-10-04 12:26:20 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,28 +2,28 @@ class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
   $install_dir = '/opt/minecraft'
 ){
-  file {$install_dir:
+  file { $install_dir:
     ensure => directory,
   }
-  file {"${install_dir}/minecraft_server.jar":
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
-    before => Service['minecraft']'
   }
+
   package {'java':
     ensure => present,
   }
   file {"${install_dir}/eula.txt":
     ensure => file,
-    content => 'eula=true',
+    content => 'eula=true'
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    content => epp('minecraft/minecraft.service',{
+    content => epp(‘minecraft/minecraft.service.epp’, {
       install_dir => $install_dir,
-    })
+    }),
   }
-  service{'minecraft':
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
